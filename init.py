bc20cad1c37aba7ed08f300366aac245d5742b0e
goodguyerik <macbook@edr27238.dip.tu-dresden.de>
2025-07-11 10:44:50 +0200

    little changes regarding concurrency

--- init.py
+++ init.py
@@ -67,7 +67,8 @@ def get_connection(service_name):
             conn = psycopg2.connect(
                 database="RetrievalSystem",
                 user="postgres",
-                host=f"db_{service_name}",#'localhost',
+                host=f"db_{service_name}"
+                #host='localhost',
                 password="root",
                 port=5432
             )
